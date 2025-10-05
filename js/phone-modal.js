8cce9bc125a3a25f3e845eac3c9c19a79a98ca7c
Shiryu <iladavidcuk3@gmail.com>
2025-10-05 17:34:42 +0300

    all done

--- js/phone-modal.js
+++ js/phone-modal.js
@@ -1,8 +1,17 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
+const menuLinks = document.querySelectorAll('.mobile-menu__item-link');
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
+
+menuLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    if (mobileMenu.classList.contains('is-open')) {
+      toggleMenu();
+    }
+  });
+});
